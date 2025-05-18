6bb2e73b3a28b85631ea295f2f014c38ab2b959a
Sandrine Ebelebe <sandrineebelebe@MacBook-Pro-de-Sandrine.local>
2025-05-18 09:56:58 +0200

    Added api modification
    # Conflicts:
    #	ecodeli-front/src/App.tsx
    #	ecodeli-front/src/components/AdvantagesSection.tsx
    #	ecodeli-front/src/components/FinalCTA.tsx
    #	ecodeli-front/src/components/Footer.tsx
    #	ecodeli-front/src/components/HeroSection.tsx
    #	ecodeli-front/src/components/ServicesSection.tsx
    #	ecodeli-front/src/components/header.tsx
    #	ecodeli-front/src/pages/Login.tsx
    #	ecodeli-front/src/pages/Register.tsx

---
+++ ecodeli-front/src/pages/About.tsx
@@ -0,0 +1,5 @@
+const About = () => {
+  return <div className="p-8 text-2xl font-bold">À propos</div>;
+};
+
+export default About;
---
+++ ecodeli-front/src/pages/Services.tsx
@@ -0,0 +1,10 @@
+const Services = () => {
+  return (
+    <>
+      <div>test</div>
+      <div className="p-8 text-2xl font-bold">À propos</div>;
+    </>
+  );
+};
+
+export default Services;
