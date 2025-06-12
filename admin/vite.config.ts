54d091742514166b0f27c07b98d0cd7dab2ae1ee
svndrine <sebelebe.se@gmail.com>
2025-06-12 11:44:47 +0200

    Docker : front bug fix

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,18 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import path from 'path'
-
+import path from 'path';
 
 export default defineConfig({
-  plugins: [ tailwindcss(), react()],
+  plugins: [react()],
+  base: '/admin/',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
-    host: "0.0.0.0", // ← important pour que Docker puisse exposer le port
-    port: 80      // ← le même que dans docker-compose
+    host: '0.0.0.0',
+    port: 80,
   },
 });
